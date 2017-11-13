da16db9958f68d3a574a4a143d5580b34cd27ec9
siwangqishiq <525647740@qq.com>
2017-11-13 17:09:33 +0800

    add a game framework

---
+++ GameTeris.py
@@ -0,0 +1,17 @@
+﻿import sys,pygame,time
+
+class Game(object):
+	SCREEN_WIDTH = 480
+	SCREEN_HEIGHT = 800
+	
+	def init(self):
+		#print("game_init")
+		pass
+		
+	def	handle_input(self , ev):
+		#print("handle_input")
+		print(ev)
+	
+	def update(self , screen):
+		#print("game update")
+		pass
---
+++ Tetris.py
@@ -0,0 +1,30 @@
+﻿import sys,pygame,time
+from GameTeris import Game
+
+def main():
+	COLOR_WHITE = (255,255,255)	
+	
+	pygame.init()
+	screen = pygame.display.set_mode((Game.SCREEN_WIDTH , Game.SCREEN_HEIGHT),0 , 32)
+	pygame.display.set_caption("Tetris Game")
+	
+	game = Game()
+	game.init()
+	
+	while True:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				sys.exit()
+			else:
+				game.handle_input(event)
+			
+		screen.fill(COLOR_WHITE)
+		game.update(screen)
+		pygame.display.flip()
+		
+		time.sleep(0.1)
+	
+	
+	
+if __name__=="__main__":
+	main()
